9513247bce5bca502d027cefa2466056ad820608
Maxur <marcelo.soto01@alumnos.ucn.cl>
2024-05-15 19:47:19 -0400

    cree los tsx que usare para listar y crear

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import { HOST_LOCAL, HOST_EMULATOR} from '@env';
 
 const ApiDelivery = axios.create({
+    //comment to easy use of the app for me
     baseURL: HOST_EMULATOR,
+    //baseURL: 'http://192.168.1.89:3380/api',
     headers: {
         'Content-Type': 'application/json',
     }
