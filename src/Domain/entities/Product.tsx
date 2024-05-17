df13c59f79597d88c6b867393f25faa47ab964da
Maxur <marcelo.soto01@alumnos.ucn.cl>
2024-05-16 23:22:18 -0400

    productfix

--- src/Domain/entities/Product.tsx
+++ src/Domain/entities/Product.tsx
@@ -1,13 +1,11 @@
 
-export interface Products {
+export interface Product {
 
     id?: string;
     name: string;
     description: string;
     price: number;
-<<<<<<< HEAD
-    categoryId: number;
-=======
 
->>>>>>> 7d92df10d115ed711a72c7646b0a40060ae1682d
+    categoryId: number;
+
 }
