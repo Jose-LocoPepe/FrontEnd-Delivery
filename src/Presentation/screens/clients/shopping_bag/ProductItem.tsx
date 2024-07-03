bef545771e13cf09ea59ccfdc4bd47e8e196c5e1
Jose Bautista <jose.bautista@alumnos.ucn.cl>
2024-07-03 19:14:18 -0400

    Carrito de compras listo, faltaria el navigation

--- src/Presentation/screens/clients/shopping_bag/ProductItem.tsx
+++ src/Presentation/screens/clients/shopping_bag/ProductItem.tsx
@@ -1,73 +1,124 @@
-import { View, Text, Button, Touchable, Image } from 'react-native'
-import React, { useState } from 'react'
-import { StackScreenProps, StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
-import { CategoryStackParamList } from '../../../navigator/tabs/admin/AdminCategoryNavigator';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { ModalConfirmation } from '../../../components/ModalConfirmation';
+import React from 'react'
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
 import { Product } from '../../../../Domain/entities/Product';
-import { useProductViewModel } from './ViewModel';
-import { ProductStackParamList } from '../../../navigator/tabs/admin/AdminProductNavigator';
-
-
-
 
 interface Props {
-
-    product: Product;
+    product: Product,
+    addItem: (product: Product) => void,
+    subtractItem: (product: Product) => void,
+    deleteItem: (product: Product) => void,
 }
 
-export const ShoppingBagItem = ({ product }: Props) => {
-    const [nameCategory, setNameCategory] = useState<string>('');
-    const [modalVisible, setModalVisible] = useState(false);
-    const { products, loading, error,firstPic,deleteProduct, setCategoryName,firstImage } = useProductViewModel(); // Call the hook to get categories and loading state
-   const navigation = useNavigation<StackNavigationProp<ProductStackParamList>>();
+export const ShoppingBagItem = ({product, addItem, subtractItem, deleteItem}: Props) => {
   return (
-    <View style={{ margin: 2,padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <View style={{width: '20%'}}>
-                <Image
-                    style={{ width: 50, height: 50, resizeMode: 'contain' }}
-                    source={{ uri: firstPic[parseInt(product.id)] ? firstPic[parseInt(product.id)] : 'https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg' }}
-                />
+    <View style={styles.container}>
+        
+        <View style={styles.productInfo}>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.title}>{product.name}</Text>
+                <Text style={styles.price}>${product.quantity! * product.price}</Text>
+            </View>
 
-                
-            </View>
-            <View style={{width: '60%'}}>
-                <Text>Nombre: {product.name}</Text>
-                <Text>Descripción: {product.description}</Text>
-                <Text>Precio: {product.price}</Text>
-                <Text>Categoría: {nameCategory}</Text>
-            </View>
-            <View style={{width:'20%', flexDirection:'row'}}>
-            <TouchableOpacity
-                    onPress={() => {
-                        if(product.id){
-                            console.log("Navigating to UpdateProductScreen with productId:", product.id)    ;
-                            navigation.navigate('UpdateProductScreen', {  product: product });
-                        }
-                    }}
-                >
+            <View style={styles.productActions}>
+                <View style={styles.actions}>
+                    <TouchableOpacity 
+                        onPress={() => subtractItem(product)}
+                        style={styles.actionLess}
+                    >
+                        <Text style={styles.actionText}>-</Text>
+                    </TouchableOpacity>
+                    
+                    <View style={ styles.quantity }>
+                        <Text style={styles.actionText}>{ product.quantity }</Text>
+                    </View>
+                    
+                    <TouchableOpacity 
+                        onPress={() => addItem(product)}
+                        style={styles.actionAdd}
+                    >
+                        <Text style={styles.actionText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity onPress={() => deleteItem(product)}>
                     <Image
-                        style={{ width: 30, height: 30, resizeMode: 'contain', marginRight: 10 }}
-                        source={require('../../../../../../assets/edit.png')}
+                        style={styles.deleteItem}
+                        source={require('../../../../../assets/trash.png')}
                     />
                 </TouchableOpacity>
-            <TouchableOpacity
-                onPress={()=> setModalVisible(true)} // Invoke the delete function with category id
-                >
-                <Image
-                 style={{width: 30, height: 30, resizeMode: 'contain'}}
-                 source={require('../../../../../../assets/trash.png')}
-                 />
-                </TouchableOpacity>
             </View>
-            <ModalConfirmation
-                modalUseState={modalVisible}
-                setModalUseState={setModalVisible}
-                action={()=>{deleteProduct(product.id!)}}
-            />
         </View>
+    </View>
   )
 }
 
-export default ProductItem
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 70,
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        marginTop: 7
+    },
+    imageContainer: {
+
+    }, 
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 15
+    },
+    productInfo: {
+        flex: 1
+    },
+    title: {
+        color: 'black',
+        fontSize: 14,
+        marginLeft: 15,
+        flex: 1
+    },
+    price: {
+       marginRight: 40,
+       fontWeight: 'bold'
+    },
+    productActions: {
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginTop: 5,
+        marginRight: 45
+    },
+    actionLess: {
+        backgroundColor: '#f2f2f2',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        alignSelf: 'center',
+        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 10
+    },
+    actionAdd: {
+        backgroundColor: '#f2f2f2',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        alignSelf: 'center',
+        borderBottomRightRadius: 10,
+        borderTopRightRadius: 10
+    },
+    actionText: {
+        color: 'black',
+        fontSize: 15
+    },
+    quantity: {
+        backgroundColor: '#f2f2f2',
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        alignSelf: 'center',
+    },
+    actions: {
+        flexDirection: 'row',
+        flex: 1
+    },
+    deleteItem: {
+        width: 25,
+        height: 25
+    }
+});
